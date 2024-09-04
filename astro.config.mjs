cd16b689208d8e03deb04f3aad3021509544338b
Confaa <juanconfaa@gmail.com>
2024-09-04 10:49:02 -0300

    [Experimental] Init proyect & first model

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { fileURLToPath } from 'url';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "@assets": fileURLToPath(new URL('./src/assets', import.meta.url)),
+        "@components": fileURLToPath(new URL('./src/components', import.meta.url)),
+        "@layouts": fileURLToPath(new URL('./src/layouts', import.meta.url)),
+      },
+    },
+  },
+});
