60730cd35bfbdb3dc232a628af9ca2a8036d5481
Confaa <juanconfaa@gmail.com>
2024-09-04 21:49:26 -0300

    [Release] First version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,3 @@
 import { defineConfig } from "astro/config";
-import { fileURLToPath } from 'url';
 
-export default defineConfig({
-  vite: {
-    resolve: {
-      alias: {
-        "@assets": fileURLToPath(new URL('./src/assets', import.meta.url)),
-        "@components": fileURLToPath(new URL('./src/components', import.meta.url)),
-        "@layouts": fileURLToPath(new URL('./src/layouts', import.meta.url)),
-      },
-    },
-  },
-});
+export default defineConfig({});
